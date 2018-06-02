5fac39c14de7c6e41dbb5e51fbf665d2d3082493
Ntim Yeboah <lylegendary@gmail.com>
2018-06-02 20:22:43 +0000

    ch(): Log queues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace App\Providers;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Queue\Events\JobProcessing;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +18,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Queue::before(function (JobProcessing $event) {
+            Log::info('Starting to process job', [
+                'connection' => $event->connectionName,
+                'job' => $event->job,
+                'payload' => $event->job->payload()
+            ]);
+       });
+
+       Queue::after(function (JobProcessed $event) {
+             Log::info('Finished processing job', [
+                'connection' => $event->connectionName,
+                'job' => $event->job,
+                'payload' => $event->job->payload()
+            ]);
+       });
+
+       Queue::failing(function (JobFailed $job) {
+            Log::info('Job failed', [
+                'connection' => $event->connectionName,
+                'job' => $event->job,
+                'exception' => $event->exception
+            ]);
+       });
     }
     
     /**
